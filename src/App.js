3f828c668151b3713a7cc77716e169f2c3d45946
qrkwang <1901829@sit.singaporetech.edu.sg>
2021-03-13 22:22:43 +0800

    routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Link, Route, Switch } from "react-router-dom";
+
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+const Home = () => (
+  <div>
+    <h2>Home Page</h2>{" "}
+    <p> This is definitely not what our app will look like</p>
+  </div>
+);
+
+const CheckBooking = () => (
+  <div>
+    <h2>Check Your Booking Here</h2>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li>
+            <Link to="/checkbooking">Check Booking</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/checkbooking">
+          <CheckBooking />
+        </Route>
+      </Switch>
     </div>
   );
 }
