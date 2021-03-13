d8dfb8804e2e5298c433c1318941e9b46ac394ba
qrkwang <1901829@sit.singaporetech.edu.sg>
2021-03-13 22:57:43 +0800

    setup nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Link, Route, Switch } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    padding: 40,
+    background: theme.background,
+    textAlign: "center",
+  },
+  title: {
+    paddingRight: "50px",
+    color: (props) => props.color,
+  },
+  link: {
+    // color: theme.color,
+    "&:hover": {
+      opacity: 0.5,
+    },
+  },
+}));
 
 const Home = () => (
   <div>
@@ -19,19 +39,38 @@ const CheckBooking = () => (
 );
 
 function App() {
+  const classes = useStyles({});
+
   return (
     <div className="App">
-      <nav className="navbar navbar-light">
-        <ul className="nav navbar-nav">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-
-          <li>
-            <Link to="/checkbooking">Check Booking</Link>
-          </li>
-        </ul>
-      </nav>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography href="/" variant="h6" className={classes.title}>
+            <Link
+              to="/"
+              style={{ textDecoration: "none", color: "white" }}
+              // href="/"
+              color="inherit"
+            >
+              Book
+            </Link>{" "}
+          </Typography>
+          <Typography
+            href="/checkbooking"
+            variant="h6"
+            className={classes.title}
+          >
+            <Link
+              to="/checkbooking"
+              style={{ textDecoration: "none", color: "white" }}
+              // href="/checkbooking"
+              color="inherit"
+            >
+              Check Bookings
+            </Link>{" "}
+          </Typography>{" "}
+        </Toolbar>
+      </AppBar>
       <Switch>
         <Route exact path="/">
           <Home />
