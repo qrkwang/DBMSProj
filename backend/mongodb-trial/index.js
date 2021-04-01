b982945f3a876d609c3a6728ba5d81236da1d80c
Benedict Lee <Benflop@users.noreply.github.com>
2021-04-02 05:34:00 +0800

    Added API Calls & Express Routing
    Added API Calls & Express Routing for MongoDB

--- backend/mongodb-trial/index.js
+++ backend/mongodb-trial/index.js
@@ -4,15 +4,33 @@ const db = require('./queries')
 
 
 
-app.get('/customer', db.getCustomers)
-
+app.get('/customer', db.getCustomers);
+app.get('/customer/customerbyid', db.getCustomersById);
 app.get('/hotel/hotellisting',db.getHotelListing);
-
+app.get('hotel/hotellistingbyid', db.getHotelListingById);
 app.get('/hotel/hotellistingdetail',db.getHotelListingDetails);
-
-app.get('/hotel/hotelreview',db.getHotelReview );
-
-app.get('/booking',db.getBooking );
+app.get('/hotel/hotellistingdetailbyid', db.getHotelListingDetailsById);
+app.get('/hotel/hotelreview', db.getHotelReview);
+app.get('/hotel/hotelreviewbyid', db.getHotelReviewById);
+app.get('/hotel/hotellistingwithdetails', db.getHotelListingWithDetails);
+app.get('/hotel/hotellistingwithdetailsbyid', db.getHotelListingWithDetailsById);
+app.get('/booking',db.getBooking);
+app.get('/booking/bookingbyid', db.getBookingById);
+app.post('/customer/createuser', db.createUser);
+app.post('/booking/createbooking', db.createBooking);
+app.post('/hotel/createhotelreview', db.createHotelReview);
+app.post('/hotel/createlistingdetails', db.createListingDetails);
+app.post('/hotel/createlisting', db.createListing);
+app.post('customer/updateuser', db.updateUser);
+app.post('/booking/updatebooking', db.updateBooking);
+app.post('/hotel/updatehotelreview', db.updateHotelReview);
+app.post('/hotel/hotellisting', db.updateListing);
+app.post('/hotel/hotellistingdetails', db.updateListingDetails);
+app.post('/booking/deletebooking', db.deleteBooking);
+app.post('/hotel/deletehotelreview', db.deleteHotelReview);
+app.post('/hotel/deletelisting', db.deleteListing);
+app.post('/hotel/deletelistingdetails', db.deleteListingDetails);
+app.post('/customer/deleteuser', db.deleteUser);
 
 
 
